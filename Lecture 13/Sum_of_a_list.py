a2866ad5708a37f8b252660bed4db5630acaedd5
Khaled <kmnafiz0710@gmail.com>
2024-10-08 16:14:39 +0600

    Update the sum of a list.

--- Lecture 13/Sum_of_a_list.py
+++ Lecture 13/Sum_of_a_list.py
@@ -3,10 +3,11 @@ def sum_list(numbers):
     summ=0
     for n in numbers:
         summ+=n
+    return summ
 
 
 Num_list=list(map(int, input("Enter numbers that you wanna sum: ").split()))
 sum=sum_list(Num_list)
-print(f"The summation of all element of this list is: ")
+print(f"The summation of all element of this list is: {sum}")
 
 # Its Final result.
